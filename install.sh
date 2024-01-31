1d412757ac864eda7d00e8e0a2e8c79b42cbab94
shoto <57732081+Shoto31@users.noreply.github.com>
2024-01-31 14:23:37 +0000

    Update install.sh
    removed and added some apps

--- install.sh
+++ install.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-
-
-sudo add-apt-repository ppa:bashtop-monitor/bashtop
-sudo add-apt-repository ppa:agornostal/ulauncher 
 sudo add-apt-repository ppa:lutris-team/lutris
-sudo add-apt-repository ppa:linrunner/tlp
- 
- sudo apt update 
- 
- # Get DEB files
- wget https://github.com/flavioislima/HeroicGamesLauncher/releases/download/v1.5.5/heroic_1.5.5_amd64.deb
+sudo apt update  
+
+# Get DEB files
+wget https://github.com/flavioislima/HeroicGamesLauncher/releases/download/v1.5.5/heroic_1.5.5_amd64.deb
  
 #install applications
 sudo apt install vim -y
@@ -17,27 +11,24 @@ sudo apt install vlc -y
 sudo apt install ubuntu-restricted-extras -y
 sudo apt install gnome-tweaks -y
 sudo apt install firefox-gnome-shell -y
-sudo apt install kitty -y
+sudo apt install alacritty -y
 sudo apt install steam -y
 sudo apt install timeshift -y
 sudo apt install synaptic -y
 sudo apt install zathura -y
-sudo apt install cargo -y
 sudo apt install flameshot -y
 sudo apt install python3-pip -y
-sudo apt-get install tlp tlp-rdw -y
- sudo apt install bashtop -y
- sudo apt install ulauncher -y
- sudo apt install lutris -y
- sudo apt install nitrogen -y
+sudo apt install ulauncher -y
+sudo apt install lutris -y
+#Install Cargo 
+curl https://sh.rustup.rs -sSf | sh
+
 
 # Install DEB files
 dpkg -i heroic_1.5.5_amd64.deb
 
 #install macchina (alternative to neofetch)
 cargo install macchina
-
-sudo tlp start
 
 # Final message
 echo All application have been installed, the script will now quit.
